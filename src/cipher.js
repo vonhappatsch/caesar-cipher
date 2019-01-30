9719fa4cb31a37ac82c570b5d6d8f11ded636a37
vonhappatsch <vonhappatsch@gmail.com>
2019-01-30 19:48:20 -0200

    Nova versão da cifra (H.E.) e mudanças no CSS

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,20 @@
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
 function encode(offset, string) {
-  let final = "";
+  let final = ""; 
 
   for (let i = 0; i < string.length; i++) {
     let letter = string.charCodeAt(i);
-    if ((letter >= 32) && (letter <= 254)) {
-      encLetter = ((letter - 32 + offset) % 222) + 32;
-      final += String.fromCharCode(encLetter);
+    if ((letter >= 65) && (letter <= 90)) {
+      encUpper = mod((letter - 65 + offset), 26) + 65;
+      final += String.fromCharCode(encUpper);
+    } else if ((letter >= 97) && (letter <= 122)) {
+      encLower = mod((letter - 97 + offset), 26) + 97;
+      final += String.fromCharCode(encLower);
+    } else {
+      final += String.fromCharCode(letter);
     }
   }
   return final;
@@ -16,28 +25,23 @@ function decode(offset, string) {
 
   for (let i = 0; i < string.length; i++) {
     let letter = string.charCodeAt(i);
-    if ((letter >= 32) && (letter <= 254)) {
-      encLetter = ((letter - 32 - offset) % 222) + 32;
-      final += String.fromCharCode(encLetter);
+    if ((letter >= 65) && (letter <= 90)) {
+      encUpper = mod((letter - 65 - offset), 26) + 65;
+      final += String.fromCharCode(encUpper);
+    } else if ((letter >= 97) && (letter <= 122)) {
+      encLower = mod((letter - 97 - offset), 26) + 97;
+      final += String.fromCharCode(encLower);
+    } else {
+      final += String.fromCharCode(letter);
     }
   }
   return final;
 }
 
-function getTextEncode() {
-  const key = parseInt(document.getElementById("offsetEncode").value);
-  const text = document.getElementById("textToEncode").value;
+function getText(textfield, offsetfield, functionToExecute, resultfield) {
+  const key = parseInt(document.getElementById(offsetfield).value);
+  const text = document.getElementById(textfield).value;
+  const result = functionToExecute(key, text);
 
-  const result = encode(key, text);
-
-  document.getElementById("encoded").innerHTML = "<b>A sua mensagem criptografada é</b>:<br> " + result;
-}
-
-function getTextDecode() {
-  const keyDec = parseInt(document.getElementById("offsetDecode").value);
-  const textDec = document.getElementById("textToDecode").value;
-
-  const result = decode(keyDec, textDec);
-
-  document.getElementById("decoded").innerHTML = "<b>A sua mensagem decifrada é</b>:<br> " + result;
+  document.getElementById(resultfield).innerHTML = "<b>O resultado é</b>:<br> " + result;
 }
--- src/tests.js
+++ src/tests.js
@@ -1,6 +1,48 @@
-// testei assim no console
-encode (7, "amor");
-decode (7, "htvy");
+let resultadoEncode = encode(7, "JÉSSICA NÃO CURTE ABACATE KKK");
 
-encode(10, "ola amiga");
-decode(10, "yvk kwsqk");
+if (resultadoEncode === "QÉZZPJH UÃV JBYAL HIHJHAL RRR") {
+    console.log("A função de criptografar funcionou! O resultado " + resultadoEncode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de criptografar flopou. O resultado " + resultadoEncode + " não é o esperado ('QÉZZPJH UÃV JBYAL HIHJHAL RRR').");
+}
+
+resultadoEncode = encode(135000, "aloha galerezzz!!11!!");
+
+if (resultadoEncode === "itwpi oitmzmhhh!!11!!") {
+    console.log("A função de criptografar funcionou! O resultado " + resultadoEncode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de criptografar flopou. O resultado " + resultadoEncode + " não é o esperado ('itwpi oitmzmhhh!!11!!').");
+}
+
+resultadoEncode = encode(2755300, "o coração da zebra não é listrado kkk");
+
+if (resultadoEncode === "q eqtcçãq fc bgdtc pãq é nkuvtcfq mmm") {
+    console.log("A função de criptografar funcionou! O resultado " + resultadoEncode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de criptografar flopou. O resultado " + resultadoEncode + " não é o esperado ('q eqtcçãq fc bgdtc pãq é nkuvtcfq mmm').");
+}
+
+
+let resultadoDecode  = decode(7, "QÉZZPJH UÃV JBYAL HIHJHAL RRR");
+
+if (resultadoDecode === "JÉSSICA NÃO CURTE ABACATE KKK") {
+    console.log("A função de decifrar funcionou! O resultado " + resultadoDecode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de decifrar flopou. O resultado " + resultadoDecode + " não é o esperado ('JÉSSICA NÃO CURTE ABACATE KKK').");
+}
+
+resultadoDecode = decode(135000, "itwpi oitmzmhhh!!11!!");
+
+if (resultadoDecode === "aloha galerezzz!!11!!") {
+    console.log("A função de decifrar funcionou! O resultado " + resultadoDecode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de decifrar flopou. O resultado " + resultadoDecode + " não é o esperado ('aloha galerezzz!!11!!').");
+}
+
+resultadoDecode = decode(2755300, "q eqtcçãq fc bgdtc pãq é nkuvtcfq mmm");
+
+if (resultadoDecode === "o coração da zebra não é listrado kkk") {
+    console.log("A função de decifrar funcionou! O resultado " + resultadoDecode + " é o esperado.");
+} else {
+    console.log("Tente novamente, a função de decifrar flopou. O resultado " + resultadoDecode + " não é o esperado ('o coração da zebra não é listrado kkk').");
+}
